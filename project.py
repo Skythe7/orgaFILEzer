8375ea7a1df836119fb87880c8ca6b810b693ac4
skythe7 <skythe761@gmail.com>
2026-07-22 19:33:09 +0700

    Folder organizer feature

--- project.py
+++ project.py
@@ -8,8 +8,8 @@ import sys
 
 parser = argparse.ArgumentParser(description="Organize, inspect, and analyze your filesystem!")
 
-parser.add_argument("path")
-parser.add_argument("options", choices=["organize", "inspect", "analyze"])
+parser.add_argument("options", choices=["organize", "inspect", "analyze"], nargs='?')
+parser.add_argument("path", nargs='?')
 
 args = parser.parse_args()
 
@@ -18,20 +18,23 @@ def main():
     if len(sys.argv) == 1:
         menu()
     else:
-        path = convert_to_path(args.path)
+        try:
+            path = convert_to_path(args.path)
 
-        match args.options:
-            case "organize":
-                folder_download_organizer(path)
-            case "inspect":
-                archive_inspector(path)
-            case "analyze":
-                project_analyzer(path)
+            match args.options:
+                case "organize":
+                    folder_organizer(path)
+                case "inspect":
+                    archive_inspector(path)
+                case "analyze":
+                    project_analyzer(path)
+        except FileNotFoundError:
+            sys.exit("File or folder does not exist!")
 
 
 def menu():
     try:
-        print("1. Folder Download Organizer")
+        print("1. Folder Organizer")
         print("2. Project Analyzer")
         print("3. Archive Inspector")
         user_choice = int(input("Options (1-3): "))
@@ -40,7 +43,7 @@ def menu():
         
         match user_choice:
             case 1:
-                folder_download_organizer(path)
+                folder_organizer(path)
             case 2:
                 project_analyzer(path)
             case 3:
@@ -50,26 +53,36 @@ def menu():
         
     except ValueError:
         sys.exit("Please insert a number!")
+    except FileNotFoundError:
+        sys.exit("File or folder does not exist!")
   
 
-def folder_download_organizer(path):
+def folder_organizer(path: Path):
+    for file in path.iterdir():
+        if file.is_file():
+            target_dir = path / f"{file.suffix.removeprefix(".")} files"
+            print(target_dir)
+            target_dir.mkdir(exist_ok=True)
+            shutil.move(file, target_dir)
+
+def project_analyzer(path: Path):
     ...
 
 
-def project_analyzer(path):
-    ...
-
-
-def archive_inspector(path):
+def archive_inspector(path: Path):
     ...
 
 
 def convert_to_path(path_string):
-    path = Path(path_string).expanduser()
-    if not path.is_absolute():
-        path = Path.home() / path
-    
-    return path
+    try:
+        path = Path(path_string).expanduser()
+        if not path.is_absolute():
+            path = Path.home() / path
+        
+        return path
+    except TypeError:
+        sys.exit("Please enter path!")
+
 
 
 if __name__ == "__main__":
