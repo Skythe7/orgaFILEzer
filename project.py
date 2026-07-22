9fb4606ab5183760c313237d719bdd643d737e70
skythe7 <skythe761@gmail.com>
2026-07-22 18:13:21 +0700

    Setup Project

---
+++ project.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import shutil
+import datetime
+import zipfile
+import argparse
+
+def main():
+    ...
+
+def folder_download_orginizer():
+    ...
+
+def local_project_analyzer():
+    ...
+
+def archive_inspector():
+    ...
